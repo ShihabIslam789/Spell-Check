bf864b0ba7f0560dc45922aee8c65e1ae579d51a
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-16 19:00:36 -0500

    seperating text from lower cases.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
+
 import nltk
 from nltk.corpus import words
 
@@ -13,10 +14,17 @@ class SpellingChecker:
         self.root.geometry("600x500")
 
         self.text = Scrolledtext(Self.root, font = ("arial",14))
-        self.text.bind("<Keyrelease>", self.check)
+        self.text.bind("<KeyRelease>", self.check)
         self.text.pack()
+
+        self.old_spaces = 0
 
         self.root.mainloop()
 
     def check(self,event):
-        print ("Hello World")
+        content = self.text.get("1.0", tk.END)
+        space_count = content.count(" ")
+        if space_count != self.old_spaces:
+            self.old_spaces = space_count
+            for word in_content.spilt(" "):
+                if re.sub(r"[^\w]", "", word.lower())
