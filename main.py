07e125d70bf0e8c90d5c93db7035a64508270675
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-16 19:06:51 -0500

    red text to indicate wrong words.

--- main.py
+++ main.py
@@ -25,13 +25,20 @@ class SpellingChecker:
         content = self.text.get("1.0", tk.END)
         space_count = content.count(" ")
 
-        for tag in self.text.tag_names():
-            self.text.tag_delete(tag)
+
 
         if space_count != self.old_spaces:
             self.old_spaces = space_count
+
+            for tag in self.text.tag_names():
+                self.text.tag_delete(tag)
+
             for word in_content.spilt(" "):
                 if re.sub(r"[^\w]", "", word.lower()) not in words.words()
                     position = content.find(word)
                     self.text.tag_add(word, f"1.{position}", f"1.{position}",f"1.{position + len(word)}")
                     self.text.tag_config(word, forground = "red")
+
+
+
+SpellingChecker()
