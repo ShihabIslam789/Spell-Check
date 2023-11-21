0630ebd14d325d20ef06e846541d5b9d05eeeb13
Shihab Islam <56773545+ShihabIslam789@users.noreply.github.com>
2023-11-20 19:11:01 -0500

    added readme update.

--- main.py
+++ main.py
@@ -26,20 +26,16 @@ class SpellingChecker:
         content = self.text.get("1.0", tk.END)
         space_count = content.count(" ")
 
-
-
         if space_count != self.old_spaces:
             self.old_spaces = space_count
 
             for tag in self.text.tag_names():
                 self.text.tag_delete(tag)
 
-            for word in content.spilt(" "):
+            for word in content.split(" "):
                 if re.sub(r"[^\w]", "", word.lower()) not in words.words():
                     position = content.find(word)
                     self.text.tag_add(word, f"1.{position}", f"1.{position}",f"1.{position + len(word)}")
-                    self.text.tag_config(word, forground = "red")
-
-
+                    self.text.tag_config(word, forground = "green")
 
 SpellingChecker()
